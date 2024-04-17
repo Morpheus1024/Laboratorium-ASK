9329773c45ff832d59ff1424e4ad4f6420a208e4
drzewo1337 <105047709+drzewo1337@users.noreply.github.com>
2024-04-17 19:30:41 +0200

    Add files via upload

---
+++ Projekt 2/ask2v3/ask2/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace ask2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        private Form2 okno1;
+        private Form3 okno2;
+        private Form4 okno3;
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(149, 329);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(217, 139);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "TEST 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(602, 329);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(217, 139);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "TEST 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1051, 329);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(217, 139);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "TEST 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1436, 853);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Ekran glowny";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+        #endregion
+    
+}
+
---
+++ Projekt 2/ask2v3/ask2/Form3.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ask2
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            stopwatch = new Stopwatch();
+            InitializeComponent();
+            czy_pierwsza = 1;
+            wystapienia = 0;
+            MessageBox.Show($"W tym zadaniu należy wcisnąc przycisk 'Start' w momencie, w którym usłyszy się dźwięk oraz zapali kolor zielony.");
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000; // Ustawienie interwału timera na 1000 ms (czyli 1 sekunda)      
+            MessageBox.Show($"To jest pierwsza próba, teraz punkty nie będą zliczane, gra rozpocznie się automatycznie.");
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Ścieżka do pliku dźwiękowego
+            string sciezkaDoDzwieku = @"C:\Users\Mateusz\Desktop\l1\ask2\ask2\bin\Debug\sound.wav";
+
+            // Tworzenie obiektu SoundPlayer z podaną ścieżką dźwiękową
+            SoundPlayer player = new SoundPlayer(sciezkaDoDzwieku);
+
+            Random dioda = new Random();
+            int randomNumber = dioda.Next(1, 9);
+            this.licznik_czasu++;
+            if (randomNumber == 1 && this.licznik_czasu < 11)
+            {
+                stopwatch.Stop();
+                this.pictureBox1.Load("zielony.png");
+                this.zmienna = 1;
+            }
+            else if (randomNumber == 2 && this.licznik_czasu < 11)
+            {
+                this.pictureBox1.Load("zielony.png");
+                player.Play();
+                stopwatch.Start();
+                this.zmienna = 2;
+            }
+            else if (randomNumber == 3 && this.licznik_czasu < 11)
+            {
+                stopwatch.Stop();
+                this.pictureBox1.Load("czerwony.png");
+                this.zmienna = 1;
+            }
+            else if (randomNumber == 4 && this.licznik_czasu < 11)
+            {
+                stopwatch.Stop();
+                this.pictureBox1.Load("czerwony.png");
+                player.Play();
+                this.zmienna = 1;
+            }
+            else if (randomNumber == 5 && this.licznik_czasu < 11)
+            {
+                stopwatch.Stop();
+                this.pictureBox1.Load("niebieski.png");
+              
+                this.zmienna = 1;
+
+            }
+            else if (randomNumber == 6 && this.licznik_czasu < 11)
+            {
+                stopwatch.Stop();
+                this.pictureBox1.Load("niebieski.png");
+                player.Play();
+
+                this.zmienna = 1;
+            }
+            else if (randomNumber == 7 && this.licznik_czasu < 11)
+            {
+                stopwatch.Stop();
+                this.pictureBox1.Load("fioletowy.png");
+
+                this.zmienna = 1;
+            }
+            else if (randomNumber == 8 && this.licznik_czasu < 11)
+            {
+                stopwatch.Stop();
+                this.pictureBox1.Load("fioletowy.png");
+                player.Play();
+
+                this.zmienna = 1;
+            }
+            else if(this.czy_pierwsza == 1)
+            {
+                stopwatch.Stop();
+                this.czy_pierwsza++;
+                timer1.Stop();
+            }
+
+            if (this.czy_pierwsza > 1)
+            {
+                if (this.licznik_czasu == 11)
+                {
+                    stopwatch.Stop();
+                    MessageBox.Show($"Test ze zliczanymi punktami rozpocznie sie po zamknieciu tego okienka.");
+                    timer1.Start();
+                }
+                if (randomNumber == 1 && this.licznik_czasu < 31)
+                {
+                    stopwatch.Stop();
+                    this.pictureBox1.Load("zielony.png");
+                    this.zmienna = 1;
+                }
+                else if (randomNumber == 2 && this.licznik_czasu < 31)
+                {
+                    stopwatch.Start();
+                    this.pictureBox1.Load("zielony.png");
+                    player.Play();
+                    this.zmienna = 2;
+                    this.wystapienia++;
+                }
+                else if (randomNumber == 3 && this.licznik_czasu < 31)
+                {
+                    stopwatch.Stop();
+                    this.pictureBox1.Load("czerwony.png");
+                    this.zmienna = 1;
+                }
+                else if (randomNumber == 4 && this.licznik_czasu < 31)
+                {
+                    stopwatch.Stop();
+                    this.pictureBox1.Load("czerwony.png");
+                    player.Play();
+                    this.zmienna = 1;
+                }
+                else if (randomNumber == 5 && this.licznik_czasu < 31)
+                {
+                    stopwatch.Stop();
+                    this.pictureBox1.Load("niebieski.png");
+                    this.zmienna = 1;
+
+                }
+                else if (randomNumber == 6 && this.licznik_czasu < 31)
+                {
+                    stopwatch.Stop();
+                    this.pictureBox1.Load("niebieski.png");
+                    player.Play();
+                    this.zmienna = 1;
+                }
+                else if (randomNumber == 7 && this.licznik_czasu < 31)
+                {
+                    stopwatch.Stop();
+                    this.pictureBox1.Load("fioletowy.png");
+                    this.zmienna = 1;
+                }
+                else if (randomNumber == 8 && this.licznik_czasu < 31)
+                {
+                    stopwatch.Stop();
+                    this.pictureBox1.Load("fioletowy.png");
+                    player.Play();
+                    this.zmienna = 1;
+                }
+                else if (this.licznik_czasu == 31)
+                {
+                    timer1.Stop();
+                    MessageBox.Show($"Skuteczność wynosi: {(float)this.licznik_dobrych_ruchow / (float)this.wystapienia * 100:F2}% a średni czas reakcji wynosi: {this.sredni_czas/this.licznik_dobrych_ruchow:F3} sek."); 
+                    this.Close();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button przycisk_X = (Button)sender;
+            if (przycisk_X.Text == "Start")
+            {
+                this.button1.BackColor = Color.LightBlue;
+                if (this.czy_pierwsza != 1)
+                {
+                    if (this.pictureBox1.ImageLocation == "zielony.png" && zmienna == 2)
+                    {
+                        this.licznik_dobrych_ruchow++;
+                        SprawdzZliczanieCzasu(pictureBox1);
+                    }
+                    else
+                    {
+                        this.licznik_dobrych_ruchow--;
+                    }
+                }
+            }
+        }
+                
+        private void SprawdzZliczanieCzasu(PictureBox pictureBox)
+        {
+            if (pictureBox.ImageLocation == "zielony.png" && zmienna == 2)
+            {
+                if (czy_pierwsza != 1)
+                {
+                    stopwatch.Stop();
+                    TimeSpan elapsed = stopwatch.Elapsed;
+                    float elapsedTimeInSeconds = (float)elapsed.TotalSeconds;
+                    czasy_reakcji.Add(elapsedTimeInSeconds);
+                    float sumaCzasow = czasy_reakcji.Sum();
+                    this.sredni_czas = sumaCzasow / czasy_reakcji.Count;
+                }
+            }
+        }
+    }
+}
